13f3887cb705810e0b5c1e43b0fd6b42b4eb5c84
hskim881028 <andykim881028@gmail.com>
2021-07-12 22:56:01 +0900

    Update prologue

--- Assets/LibUnity/Frontend/Scripts/Game/Prologue.cs
+++ Assets/LibUnity/Frontend/Scripts/Game/Prologue.cs
@@ -1,20 +1,45 @@
-using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LibUnity.Frontend
 {
     public class Prologue : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Button skipButton;
+        [SerializeField] private string synopsis = "스토리를 넣어주세요"; 
+        [SerializeField] private float typingSpeed = 0.1f;
+
+        private Coroutine _coroutine;
+        private bool _isDone;
+
         private void Start()
         {
-            StartCoroutine(LoadLobby());
-        }
+            _coroutine = StartCoroutine(TextTyper.Play(text, synopsis, typingSpeed, () =>
+            {
+                _isDone = true;
+                SceneLoader.Instnace.Unload("Prologue");
+                SceneLoader.Instnace.Load("Lobby");
+            }));
 
-        private static IEnumerator LoadLobby()
-        {
-            yield return new WaitForSeconds(2.0f);
-            SceneLoader.Instnace.Unload("Prologue");
-            SceneLoader.Instnace.Load("Lobby");
+            skipButton.onClick.AddListener(() =>
+            {
+                if (_isDone)
+                {
+                    SceneLoader.Instnace.Unload("Prologue");
+                    SceneLoader.Instnace.Load("Lobby");
+                    return;
+                }
+
+                if (_coroutine != null)
+                {
+                    StopCoroutine(_coroutine);
+                }
+
+                text.text = synopsis;
+                _isDone = true;
+            });
         }
     }
 }
