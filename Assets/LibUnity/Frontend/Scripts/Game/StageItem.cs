c5c9c440615318b915fbe3c1b282fcc97a8d1302
hskim881028 <andykim881028@gmail.com>
2021-07-12 21:43:47 +0900

    Add stage mockup

--- Assets/LibUnity/Frontend/Scripts/Game/StageItem.cs
+++ Assets/LibUnity/Frontend/Scripts/Game/StageItem.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,7 +9,12 @@ namespace LibUnity.Frontend
     public class StageItem : UIBehaviour
     {
         [SerializeField] private Button button;
-        [SerializeField] private Text stage;
+        [SerializeField] private TextMeshProUGUI stage;
+        [SerializeField] private Image background;
+        [SerializeField] private List<Sprite> map;
+
+        private const float Degree = 18;
+        private const float Gap = 300;
 
         protected override void Awake()
         {
@@ -21,7 +28,11 @@ namespace LibUnity.Frontend
 
         public void UpdateItem(int index)
         {
-            stage.text = $"스테이지 : {index}";
+            background.sprite = map[index % 20];
+            stage.text = index.ToString();
+            var sinValue = Mathf.Sin(Degree * index * Mathf.Deg2Rad);
+            var x = sinValue * sinValue * Gap;
+            button.transform.localPosition = new Vector2(x, button.transform.localPosition.y);
         }
     }
 }
