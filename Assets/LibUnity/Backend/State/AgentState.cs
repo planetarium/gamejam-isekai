c626c678e93e8b21750b5727e8ab753c4a0f021c
Yang Chun Ung <qooraven@gmail.com>
2021-07-13 12:12:28 +0900

    Introudce conquest

--- Assets/LibUnity/Backend/State/AgentState.cs
+++ Assets/LibUnity/Backend/State/AgentState.cs
@@ -1,16 +1,35 @@
-﻿using Bencodex.Types;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bencodex.Types;
 using Libplanet;
 
 namespace LibUnity.Backend.State
 {
+    [Serializable]
     public class AgentState : BaseState
     {
+        public List<int> ClearedStageList = new List<int>();
         public AgentState(Address address) : base(address)
         {
         }
 
         public AgentState(Dictionary serialized) : base(serialized)
         {
+            ClearedStageList = serialized["c"].ToList(c => c.ToInteger());
+        }
+
+        public override IValue Serialize()
+        {
+            return new Dictionary(new Dictionary<IKey, IValue>
+            {
+                [(Text)"c"] = new List(ClearedStageList.Select(s => s.Serialize()))
+            }.Union((Dictionary) base.Serialize()));
+        }
+
+        public void Add(int stageId)
+        {
+            ClearedStageList.Add(stageId);
         }
     }
 }
